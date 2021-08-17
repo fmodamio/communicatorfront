680c455eba234029ecc34c3a4077ca664d4d858f
Fernando <fernando.modamio@gmail.com>
2021-08-17 21:17:36 +0200

    cargado template

--- communicatorfront/src/App.vue
+++ communicatorfront/src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet">
+  <Inicial></Inicial>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Inicial from './components/Inicial.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Inicial
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h2 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: aqua;
+  font-family: 'Roboto', sans-serif;
+}
+
+.cita{
+  text-align: right;
+  color: aqua;
+  font-family: 'Roboto', sans-serif;
+  margin-right: 150px;
+  margin-left: 500px;
+  margin-top: 80px;
+  margin-bottom: 0px;
+  font-weight:100;
+}
+
+.autor{
+  text-align: right;
+  color: aqua;
+  font-family: 'Roboto', sans-serif;
+  margin-right: 150px;
+  margin-left: 150px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+}
+
+p {
+  text-align: left;
+  color: aqua;
+  font-family: 'Roboto', sans-serif;
+  margin-right: 150px;
+  margin-left: 150px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+body{
+  background-color: #030119;
 }
 </style>
