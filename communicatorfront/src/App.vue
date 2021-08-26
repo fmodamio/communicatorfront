cfd4b966c418690c1b6a5c9939345fa865dee02d
Fernando <fernando.modamio@gmail.com>
2021-08-26 16:01:18 +0200

    corregido css para moviles

--- communicatorfront/src/App.vue
+++ communicatorfront/src/App.vue
@@ -28,8 +28,9 @@ h2 {
   color: aqua;
   font-family: 'Roboto', sans-serif;
 }
-
-.cita{
+/*En movil los margenes no funcionan bien*/
+/*Si se quitan, en movil bien, en PC mal*/
+.cita.desk{
   text-align: right;
   color: aqua;
   font-family: 'Roboto', sans-serif;
@@ -38,9 +39,10 @@ h2 {
   margin-top: 80px;
   margin-bottom: 0px;
   font-weight:100;
+  display: block;
 }
 
-.autor{
+.autor.desk{
   text-align: right;
   color: aqua;
   font-family: 'Roboto', sans-serif;
@@ -48,16 +50,80 @@ h2 {
   margin-left: 150px;
   margin-top: 0px;
   margin-bottom: 0px;
+  display: block;
 }
 
-p {
+.cita.mobile{
+  display: none;
+}
+
+.autor.mobile{
+  display: none;
+}
+
+p.desk {
   text-align: left;
+  text-align: justify;
+  text-justify: inter-word;
   color: aqua;
   font-family: 'Roboto', sans-serif;
   margin-right: 150px;
   margin-left: 150px;
   margin-top: 40px;
   margin-bottom: 40px;
+  display: block;
+}
+
+p.mobile{
+  display: none;
+}
+
+@media screen and (max-width: 768px) {
+
+  .cita.desk{
+    display: none;
+  }
+
+  .autor.desk{
+    display: none;
+  }
+
+  .cita.mobile{
+    text-align: right;
+    color: aqua;
+    font-family: 'Roboto', sans-serif;
+    font-weight:100;
+    margin-right: 30px;
+    margin-left: 30px;
+    display: block;
+  }
+
+  .autor.mobile{
+    text-align: right;
+    color: aqua;
+    font-family: 'Roboto', sans-serif;
+    margin-right: 30px;
+    margin-left: 30px;
+    display: block;
+  }
+
+  p.desk {
+    display: none;
+  }
+
+  p.mobile {
+    text-align: left;
+    text-align: justify;
+    text-justify: inter-word;
+    color: aqua;
+    font-family: 'Roboto', sans-serif;
+    margin-right: 10px;
+    margin-left: 10px;
+    margin-top: 40px;
+    margin-bottom: 40px;
+    display: block;
+  }
+
 }
 
 body{
